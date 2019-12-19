090e93640cf0fd97343ed25cbcefd27631251be0
Olu D Gbadebo <odgbadebo@gmail.com>
2019-12-18 23:44:08 -0700

    filters are functioning now

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+/**
+ * Fetches songs from url
+ * @returns {Promise<JSON[]|string>} Returns list of songs on success or error message on failure
+ */
 function getSongs() {
   return new Promise( (resolve, reject) => {
     let url = 'https://s3.us-west-1.amazonaws.com/coding-exercise/songs';
     fetch(url).then(async response => {
       if (!response.ok) reject(`There was a problem fetching data from url ${url}`);
       resolve(await response.json())
-      // resolve(null);
     }).catch(reject);
   });
 }
 
+/**
+ * Clears songs and display error message
+ * @param {string} error - error message
+ */
 function displayError(error) {
   if (error) {
     document.querySelector('div#songs').classList.add('hidden');
@@ -18,10 +25,50 @@ function displayError(error) {
   }
 }
 
-function activateFilterButtons() {
-// activate filter buttons
-//  TODO:
-  document.querySelector('nav#filters').classList.remove('hidden');
+function sortBy(prop) {
+  // source: https://www.c-sharpcorner.com/UploadFile/fc34aa/sort-json-object-array-based-on-a-key-attribute-in-javascrip/
+  return function(a, b) {
+    if (a[prop] > b[prop]) {
+      return 1;
+    } else if (a[prop] < b[prop]) {
+      return -1;
+    }
+    return 0;
+  }
+}
+
+
+/**
+ * creates filter buttons
+ * @param {Array.<JSON>} songs - songs to apply filter to. Should be consistent with the headers in the songs table
+ * @param {Array.<string>} filter_keys - types of filter to apply to songs
+ */
+function activateFilterButtons(songs, filter_keys) {
+  let nav = document.querySelector('nav#filters');
+  nav.classList.remove('hidden');
+
+  let table_for_songs = document.querySelector('div#songs table');
+  let table_headers = table_for_songs.childNodes[0];
+
+  for (let key of filter_keys) {
+  //  create button
+    let button_for_key_filter = document.createElement('button');
+    button_for_key_filter.setAttribute('id', `sort_by_${key}`);
+    button_for_key_filter.innerText = key;
+
+  //  add listener to button
+    button_for_key_filter.addEventListener('click', ev => {
+      songs.sort(sortBy(key));
+      let song_rows = createTableRows(songs, filter_keys);
+      // remove all rows except header row in table
+      table_for_songs.innerHTML = '';
+      table_for_songs.appendChild(table_headers);
+      song_rows.forEach(row => table_for_songs.appendChild(row));
+    });
+
+  //  add button to nav
+    nav.appendChild(button_for_key_filter);
+  }
 }
 
 function arrayIntersection(a, b) {
@@ -33,10 +80,36 @@ function arrayIntersection(a, b) {
   });
 }
 
+/**
+ * create table row tags
+ * @param {Array.<JSON>} rows - array of values to display in each row
+ * @param {Array.<string>} headers - keys for each block on each row
+ * @returns {HTMLTableRowElement[]}
+ */
+function createTableRows(rows, headers) {
+  if (!rows || !headers) {
+    return []
+  }
+
+  return rows.map(row => {
+    let row_tag = document.createElement('tr');
+
+    for (let key of headers) {
+      let row_attribute = document.createElement('td');
+      row_attribute.innerText = row[key];
+      row_tag.appendChild(row_attribute)
+    }
+
+    return row_tag
+  });
+}
+
+/**
+ * create and append table tag for songs provided
+ * @param {Array.<JSON>} songs - array of songs to display
+ */
 function displaySongs(songs) {
   if (songs) {
-    console.log(songs);
-
   //  find common keys in list of jsons
     let common_keys = songs.reduce(
       (accumulator, value) => arrayIntersection(accumulator, Object.keys(value)),
@@ -59,24 +132,16 @@ function displaySongs(songs) {
 
     table_for_songs.appendChild(songs_table_header);
 
-    for (let song of songs) {
-      let song_row_tag = document.createElement('tr');
-
-      for (let key of common_keys) {
-        let song_attribute = document.createElement('td');
-        song_attribute.innerText = song[key];
-        song_row_tag.appendChild(song_attribute)
-      }
-
-      table_for_songs.appendChild(song_row_tag);
-    }
+  //  add each song as a row in table that displays songs
+    let song_rows = createTableRows(songs, common_keys);
+    song_rows.forEach(row => table_for_songs.appendChild(row));
 
   //  display songs in html
     let destination = document.querySelector('div#songs');
     destination.innerHTML = '';
     destination.appendChild(table_for_songs);
 
-    activateFilterButtons()
+    activateFilterButtons(songs, common_keys);
   }
 }
 
